567815aa734976d2cb7f832ad64c04c6bb5e874a
Iulia Luta <lutaiulia@gmail.com>
2020-08-31 13:15:57 +0100

    settings bug

--- source/libraries/deployments.js
+++ source/libraries/deployments.js
@@ -3,8 +3,8 @@
 
 var uplink = require ('./uplink');
 var debug = require ('debug')('wylidorin:app:server:deployments');
-//var child_process = require ('child_process');
-//var settings = require ('./settings');
+var child_process = require ('child_process');
+var settings = require ('./settings');
 var _ = require ('lodash');
 var Docker =  require('dockerode');
 
