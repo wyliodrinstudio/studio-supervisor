92c05abd05953bbec415c30f5fd90a1835d00d0b
Iulia Luta <lutaiulia@gmail.com>
2020-08-31 10:59:30 +0100

    changes

--- source/libraries/deployments.js
+++ source/libraries/deployments.js
@@ -15,6 +15,26 @@ var deployments = null;
 var docker = new Docker();
 
 debug ('Registering for tag dep');
+
+function listofcontainers(containers)
+{
+	let containerstosend = [];
+
+	for (let container of containers)
+	{
+		containerstosend.push({
+			name: container.Names[0],
+			ID: container.Id,
+			image: container.Image,
+			// command: container.Command,
+			// state: container.State,
+			status: container.Status
+		})
+
+	} 
+	return containerstosend;
+	
+}
 uplink.tags.on ('dep', function (c)
 {
 	if (c.a === 'run')
@@ -22,17 +42,18 @@ uplink.tags.on ('dep', function (c)
 		if (deployments === null)
 		{
 			docker.listContainers({all: true}, function(err, containers) {
-				console.log('ALL: ' + containers.length);
-				uplink.send('dep', containers);
+				
+				uplink.send('dep', listofcontainers(containers));
+				//console.log('ALL: ' + containers.length);
 			});
 
 			var s = 5000;
-			if(_.isNumber(p.s)) s = p.s*1000;
+			if(_.isNumber(c.s)) s = c.s*1000;
 
 			deployments = setInterval(function()
 			{
 				docker.listContainers({all: true}, function(err, containers) {
-					uplink.send('dep', containers);
+					uplink.send('dep', listofcontainers(containers));
 				});
 
 			},s)
