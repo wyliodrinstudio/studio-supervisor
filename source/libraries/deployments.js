04ad0f944357332bd7b574dbd71bf8b65109685c
Iulia Luta <lutaiulia@gmail.com>
2020-09-25 09:27:37 +0100

    added title to the containers

--- source/libraries/deployments.js
+++ source/libraries/deployments.js
@@ -6,27 +6,34 @@ var debug = require ('debug')('wylidorin:app:server:deployments');
 
 var _ = require ('lodash');
 var Docker =  require('dockerode');
-
-//console.log ('deploy');
-
 var deployments = null;
-
 var docker = new Docker();
-
 debug ('Registering for tag dep');
 
 function listofcontainers(containers)
 {
 	let containerstosend = [];
+	let title = null;
 
 	for (let container of containers)
 	{
-		
+		//umbla aici
+		for(let label in container.Labels)
+		{
+			if(label.substring(0,6) === 'studio')
+			{
+				title = container.Labels.studio;
+			}
+			else{
+				title = container.Names[0].substring(1);
+			}
+		}
+
 		containerstosend.push({
 			name: container.Names[0].substring(1),
 			ID: container.Id,
 			image: container.Image,
-			// command: container.Command,
+			title:title,
 			state: container.State,
 			status: container.Status
 		});
@@ -44,7 +51,6 @@ uplink.tags.on ('dep', function (c)
 			docker.listContainers({all: true}, function(err, containers) {
 				
 				uplink.send('dep', listofcontainers(containers));
-				//console.log('ALL: ' + containers.length);
 			});
 
 			var s = 5000;
@@ -57,8 +63,6 @@ uplink.tags.on ('dep', function (c)
 				});
 
 			},s);
-			
-           
 		}
 	}
 	else
