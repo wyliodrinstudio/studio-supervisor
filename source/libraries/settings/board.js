b196c7784a8d0ce212ebf6e0be7acacac1dcb446
Alexandru Radovici <msg4alex@gmail.com>
2020-11-15 13:11:45 +0200

    format

--- source/libraries/settings/board.js
+++ source/libraries/settings/board.js
@@ -6,78 +6,78 @@ var board = {
 	{
 		linux:
 		{
-			serial:'/dev/ttyS0',
-			firmware:'/Arduino/src/Arduino.ino',
+			serial: '/dev/ttyS0',
+			firmware: '/Arduino/src/Arduino.ino',
 			firmware_makefile: '',
-			nettype:'iwconfig',
-			shell:'bash',
-			avahi:'publish',
-			capabilities: {'nodejs':true, 'python':true, 'visual':true, 'shell':true, 'csharp':true}
+			nettype: 'iwconfig',
+			shell: 'bash',
+			avahi: 'publish',
+			capabilities: { 'nodejs': true, 'python': true, 'visual': true, 'shell': true, 'csharp': true }
 		},
 		windows:
 		{
-			serial:'/dev/ttyAMA0',
-			firmware:'\\Arduino\\src\\Arduino.ino',
+			serial: '/dev/ttyAMA0',
+			firmware: '\\Arduino\\src\\Arduino.ino',
 			firmware_makefile: '',
-			nettype:'',
-			shell:'powershell.exe',
-			avahi:'publish',
-			capabilities: {'nodejs':true, 'visual':true, 'powershell':true}
+			nettype: '',
+			shell: 'powershell.exe',
+			avahi: 'publish',
+			capabilities: { 'nodejs': true, 'visual': true, 'powershell': true }
 		}
 	},
 	'picopi':
 	{
 		linux:
 		{
-			firmware:'/Arduino/src/Arduino.ino',
+			firmware: '/Arduino/src/Arduino.ino',
 			firmware_makefile: '',
-			nettype:'iwconfig',
-			shell:'bash',
-			avahi:'publish',
-			capabilities: {'nodejs':true, 'python':true, 'visual':true, 'shell':true, 'csharp':true}
+			nettype: 'iwconfig',
+			shell: 'bash',
+			avahi: 'publish',
+			capabilities: { 'nodejs': true, 'python': true, 'visual': true, 'shell': true, 'csharp': true }
 		}
 	},
 	'beagleboneblack':
 	{
-		serial:'/dev/ttyGS0',
-		firmware:'',
-		firmware_makefile: ''
-		nettype:'iwconfig',
-		shell:'bash',
-		avahi:'publish',
-		capabilities: {'nodejs':true, 'python':true, 'visual':true, 'shell':true, 'csharp':true, 'streams':true}
+		serial: '/dev/ttyGS0',
+		firmware: '',
+		firmware_makefile: '',
+		nettype: 'iwconfig',
+		shell: 'bash',
+		avahi: 'publish',
+		capabilities: { 'nodejs': true, 'python': true, 'visual': true, 'shell': true, 'csharp': true, 'streams': true }
 	},
 	'udooneo':
 	{
-		serial:'/dev/ttyGS0',
-		firmware:'/Arduino/Arduino.ino',
-		firmware_makefile: ''
-		nettype:'nm',
-		shell:'bash',
-		avahi:'publish',
-		capabilities: {'nodejs':true, 'python':true, 'visual':true, 'shell':true, 'csharp':true, 'streams':true}
+		serial: '/dev/ttyGS0',
+		firmware: '/Arduino/Arduino.ino',
+		firmware_makefile: '',
+		nettype: 'nm',
+		shell: 'bash',
+		avahi: 'publish',
+		capabilities: { 'nodejs': true, 'python': true, 'visual': true, 'shell': true, 'csharp': true, 'streams': true }
 	},
 	'arduinoyun':
 	{
-		serial:null,
-		firmware:'/Arduino/Arduino.ino',
-		firmware_makefile: ''
-		nettype:'iwconfig',
-		shell:'sh',
-		avahi:'restart',
-		capabilities: {'nodejs':true, 'python':true, 'visual':true, 'shell':true, 'streams':true}
+		serial: null,
+		firmware: '/Arduino/Arduino.ino',
+		firmware_makefile: '',
+		nettype: 'iwconfig',
+		shell: 'sh',
+		avahi: 'restart',
+		capabilities: { 'nodejs': true, 'python': true, 'visual': true, 'shell': true, 'streams': true }
 	},
 	'dragonboard':
 	{
 		windows:
 		{
-			serial:'/dev/ttyAMA0',
-			firmware:'',
+			serial: '/dev/ttyAMA0',
+			firmware: '',
 			firmware_makefile: '',
-			nettype:'',
-			shell:'powershell.exe',
-			avahi:'publish',
-			capabilities: {'nodejs':true, 'visual':true, 'powershell':true}
+			nettype: '',
+			shell: 'powershell.exe',
+			avahi: 'publish',
+			capabilities: { 'nodejs': true, 'visual': true, 'powershell': true }
 		}
 	},
 };
